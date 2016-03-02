c5cf07b324d64ef4e65361a6e6673f982edf0149
Bianca Hernandez <hernandezbk22@gmail.com>
2016-03-02 14:26:08 -0800

    demo image to practice functions on and Mosaic file that gets image and resizes need t finish making image into tiles.

---
+++ Mosaic.py
@@ -0,0 +1,19 @@
+from PIL import Image
+
+myImage = Image.open("davidTennant.jpg")
+myImage.load()
+width = 500
+height = 325
+size = (50, 50)
+myImage = myImage.resize((500, height), Image.ANTIALIAS)
+myImage.show()
+
+tileWidth = 50
+numOfTilesVert = height / tileWidth
+numOfTilesHoriz = width / tileWidth
+print numOfTiles
+
+tile[10][10]
+for y in range(10)
+    for x in range(10)
+
