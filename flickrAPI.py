8303afaddda22a55da6cf85b0f4bf68a66b62aa9
Jack Platten <jack@plttn.me>
2016-03-09 15:38:39 -0800

    Add base of API calling

--- flickrAPI.py
+++ flickrAPI.py
@@ -4,3 +4,18 @@ import requests
 keyFile = open('key.txt', 'r')
 
 APIkey = keyFile.read()
+
+
+# APIendpoint = 'https://api.flickr.com/services/rest/?method=flickr.test.echo&api_key=' + APIkey + '&tags=fairies'
+#
+# print APIendpoint
+#
+# searchPictures = requests.get(APIendpoint)
+#
+# print searchPictures.text
+#
+# url = 'http://placehold.it/350x150'
+# response = requests.get(url, stream=True)
+# with open('img.png', 'wb') as out_file:
+# 	shutil.copyfileobj(response.raw, out_file)
+# del response
