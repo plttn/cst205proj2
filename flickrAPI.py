afd5b469d129c2654f42f63e7bda8e88dd186af4
Jack Platten <jack@plttn.me>
2016-03-14 14:37:59 -0700

    Fix API for real this time

--- flickrAPI.py
+++ flickrAPI.py
@@ -3,19 +3,13 @@ import requests
 #import key from untracked file
 keyFile = open('key.txt', 'r')
 
-APIkey = keyFile.read()
+APIkey = keyFile.readline().strip()
 
 
-# APIendpoint = 'https://api.flickr.com/services/rest/?method=flickr.test.echo&api_key=' + APIkey + '&tags=fairies'
-#
-# print APIendpoint
-#
-# searchPictures = requests.get(APIendpoint)
-#
-# print searchPictures.text
-#
-# url = 'http://placehold.it/350x150'
-# response = requests.get(url, stream=True)
-# with open('img.png', 'wb') as out_file:
-# 	shutil.copyfileobj(response.raw, out_file)
-# del response
+APIendpoint = 'https://api.flickr.com/services/rest/?method=flickr.test.echo&api_key=' + APIkey + '&tags=fairies'
+
+print APIendpoint
+
+searchPictures = requests.get(APIendpoint)
+
+print searchPictures.text
