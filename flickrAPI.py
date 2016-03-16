d44b9aedb8799adf382bdbdd0dd51d7bd57a8138
Jack Platten <jack@plttn.me>
2016-03-16 15:15:16 -0700

    Add implementation of flickr API

--- flickrAPI.py
+++ flickrAPI.py
@@ -1,12 +1,15 @@
 import requests
 import re
+from PIL import Image
+from StringIO import StringIO
+import os
 
 
 #import key from untracked file
 keyFile = open('key.txt', 'r')
 APIkey = keyFile.readline().strip()
 
-def SearchFlickr( tag ):
+def image_process( tag ):
 	APIendpoint = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + APIkey + '&tags=' + tag
 	searchResults = requests.get(APIendpoint)
 
@@ -20,5 +23,17 @@ def SearchFlickr( tag ):
 	for i, c, f, r in zip(ids, secrets, farms, servers):
 		photoURLs.append('https://farm' + f + '.staticflickr.com/' + r + '/' + i + '_' + c +'.jpg')
 
+	dirName = os.path.join ("tiles", tag)
+
+	if not os.path.exists(dirName):
+		os.makedirs(dirName)
+
+
+	increment = 1
 	for url in photoURLs:
-		print url
+		r = requests.get(url)
+		image = Image.open(StringIO(r.content))
+		path = os.path.join(dirName, str(increment)) + ".png"
+		image = image.resize((25,25))
+		image.save(path, "PNG")
+		increment += 1
